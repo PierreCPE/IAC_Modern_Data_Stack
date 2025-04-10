5465b43ab4687ea7374acec5662554e691b07d26
PierreCPE <pierre.gosson@cpe.fr>
2025-04-10 14:20:39 +0200

    Remove Airbyte Terraform configuration and associated files
    - Deleted the development and production environment configurations for Airbyte, including main.tf, variables.tf, and terraform.tfvars files.
    - Removed Docker provider files and associated documentation, including README.md and LICENSE.
    - Cleaned up all related modules and outputs for Airbyte and network configurations.
    - Removed Terraform state files and backups to ensure a clean slate for future configurations.

--- ingest/airbyte/terraform_airbyte_ingestion/terraform-airbyte-ingestion/modules/airbyte/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "airbyte_url" {
-  value = "http://${docker_container.airbyte.name}:8000"
-}
-
-output "postgres_url" {
-  value = "postgresql://${docker_container.postgres.name}:5432/mydatabase"
-}
